3f8628cd49579c3aed28585aa9bd3c5b908e39fd
amaragh <mercer.alecia@gmail.com>
2022-11-13 16:38:33 -0600

    add comment-routes:

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: [
+            'id',
+            'comment_text',
+            'created_at'
+        ],
+    })
+});
+
+// add withauth later
+router.post('/', (req, res) => {
+    // check the session
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // use the id from the session - make sure to update this
+            user_id: req.body.user_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+
+});
+
+module.exports = router;
