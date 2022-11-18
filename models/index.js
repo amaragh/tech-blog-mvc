d8e51dc07d9df88dcd1109707f33523a6d96f27c
amaragh <mercer.alecia@gmail.com>
2022-11-18 16:48:16 -0600

    correct associations for delete post

--- models/index.js
+++ models/index.js
@@ -6,24 +6,31 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-
 User.hasMany(Comment, {
     foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+
+
+
 
 module.exports = { User, Post, Comment };
